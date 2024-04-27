b45575dc8c92b90b95ac4337d85cf99a4028c486
Ruhao Tian <Ruhaotian.contact@gmail.com>
2024-04-27 11:05:22 -0700

    Added .sh files.

---
+++ bin/dataset.sh
@@ -0,0 +1,14 @@
+# Test Dataset
+meshName=Cube_4x4x4_E1
+pathToMesh=../volume/tbl_2_cubes_compare/$meshName.vol
+pieceNumber=4
+difficulty=4
+seed=50
+
+../bin/High-LevelPuzzle_perf $pathToMesh $pieceNumber $difficulty $seed
+
+# rename the output file
+# rename seedpath.json to seedpath_$meshName.json
+mv seedpath.json seedpath_$meshName.json
+# rename blockpath.json to blockpath_$meshName.json
+mv blockpath.json blockpath_$meshName.json
